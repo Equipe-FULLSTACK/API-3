d48f2f52ce98f764e2b56eb74b462f38d0f9444e
leonardo <leonardo.gazolamedeiros@gmail.com>
2023-09-23 13:55:08 -0300

    ultima atualização da pagina(meio quebrado)

--- api/src/main.tsx
+++ api/src/main.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
+
+
 import './index.css'
 import Navbar from './components/navbar/navbar.tsx'
-import Sidebar from './components/sidebar/sidebar.tsx'
 import Processos from './components/processos/processos.tsx'
-import Footer from './components/footer/footer.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+
     <Navbar />
-    <Sidebar />
+    <body>
     <Processos />
-    <Footer />
+    
+    </body>
   </React.StrictMode>,
 )
