df02e1539f6483fde5f23ff251b97cb43ca871e0
juliano <julianoprado98@gmail.com>
2023-10-15 16:15:08 -0200

    tela de Cadastro de usuário

--- api/src/pages/Cadastro.tsx
+++ api/src/pages/Cadastro.tsx
@@ -1,18 +1,53 @@
 export function Cadastro() {
     return (
-        <div>
-        <h1>Cadastro de usuário</h1>
-            <p>Aqui fica uma breve apresentação da página</p>
-            <div>userName</div>
-            <input type="text"/>
-            <div>userTag</div>
-            <input type="text"/>
-            <div>userEmail</div>
-            <input type="text"/>
-            <div>Digite uma senha</div>
-            <input type="text"/>          
-            <button>Cadastrar</button>
-            
+        <div className="login-containerC" id="fundo">
+        <div className="login-imagemC">
+            <img src="/./icons/logo_ionic_health.png" alt="" />
+        </div>
+        <div className="login-formC">
+            <div className="login-header-mainC">
+                Cadastro de usuário
             </div>
+            <div className="login-header-secondC">
+                Crie sua conta e aproveite nossos serviços
+            </div>
+            <form className="login-form-containerC" action="post">
+
+            <div className="login-form-containerC">
+                    <p>Nome de usuário</p>
+                </div>
+
+                <input className="login-input" type="text" name="login" id="login" placeholder="Nome"/> 
+
+
+            <div className="login-form-containerC">
+                    <p>Digite um Apelido</p>
+                </div>
+
+                <input className="login-inputC" type="text" name="apelido" id="apelido" placeholder="Apelido"/>
+
+
+                <div className="login-form-containerC">
+                    <p>Digite seu E-mail</p>
+                </div>
+
+                <input className="login-input" type="text" name="email" id="email" placeholder="E-mail"/>
+
+
+                <div className="login-form-containerC">
+                    <p>Senha</p>
+                </div>
+
+                <input type="text" name="senha" id="senha" placeholder="Senha"/>
+
+
+                <button type="submit">Cadastrar</button>
+
+            </form>
+            <div className="login-criar-contaC">
+                Já possui uma conta? <a href="http://localhost:5173/">Entrar</a>
+            </div>
+        </div>
+    </div>
     )
 }
