e6fc9ce09468d5b961425d8a8503833ec2a5e109
juliano <julianoprado98@gmail.com>
2023-10-15 01:37:16 -0200

    juliano

--- api/src/pages/Cadastro.tsx
+++ api/src/pages/Cadastro.tsx
@@ -1,5 +1,16 @@
 export function Cadastro() {
     return (
-        <h1>Hello World!</h1>
+        <div>
+        <h1>Cadastro de usuário</h1>
+            <div>userName</div>
+            <input type="text"/>
+            <div>userTag</div>
+            <input type="text"/>
+            <div>userEmail</div>
+            <input type="text"/>
+            <div>Digite uma senha</div>
+            <input type="text"/>          
+            <button>Cadastrar</button>
+            </div>
     )
 }
